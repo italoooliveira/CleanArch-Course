ac0a7fd0b19361d7bca0dc6733eec6e37f9284ab
italoooliveira <italo.oli12@gmail.com>
2022-04-17 19:34:41 -0300

    Init

---
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
